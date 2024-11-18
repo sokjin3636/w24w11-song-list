36946eff520b71096f3a55ccc5cdd90825b34948
sokjin3636 <sokjin3636@gmail.com>
2024-11-18 18:45:28 +0900

    🎉 프로젝트 시작

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <h1>React 프로그래밍</h1>
+  )
+}
+export default App
+
