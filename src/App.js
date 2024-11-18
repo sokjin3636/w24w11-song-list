8d4b1d460761eb3c089f040d5f116a7213e0a2b6
sokjin3636 <sokjin3636@gmail.com>
2024-11-18 18:46:41 +0900

    💩 노래 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div>
       <Header />
+      <Playlist />
+      <Container />
+      <Container2 />
+      <Container3 />
     </div>
   )
 }
@@ -14,5 +17,48 @@ function Header() {
     <h1>React 프로그래밍</h1>
   )
 }
-export default App
 
+function Playlist() {
+  return (
+    <div className="playlist">프로그래밍하면서 듣고 싶은 노래</div>
+  )
+}
+
+function Container() {
+  return (
+    <div className="container">
+      <a href="https://www.youtube.com/results?search_query=사랑에 연습이 있었다면">
+        <img
+          src="https://picsum.photos/600/150?random=1"
+          alt="랜덤 이미지 1" />
+        <div className="song-title">사랑에 연습이 있었다면</div>
+      </a>
+    </div>
+  )
+}
+
+function Container2() {
+  return (
+    <div className="container">
+      <a href="https://www.youtube.com/results?search_query=사건의 지평선">
+        <img
+          src="https://picsum.photos/600/150?random=2"
+          alt="랜덤 이미지 2" />
+        <div className="song-title">사건의 지평선</div>
+      </a>
+    </div>
+  )
+}
+
+function Container3() {
+  return (
+    <div className="container">
+      <a href="https://www.youtube.com/results?search_query=사랑은 늘 도망가">
+        <img src="https://picsum.photos/600/150?random=3" alt="랜덤 이미지 3" />
+        <div className="song-title">사랑은 늘 도망가</div>
+      </a>
+    </div>
+  )
+}
+
+export default App
